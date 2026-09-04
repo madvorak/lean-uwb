47f481c4cece95d6eee878257831fa0c40f57658
Martin Dvorak <martin.dvorak@matfyz.cz>
2026-09-04 08:51:35 +0200

    final proof based on generalizing

--- LeanUWB/Class13.lean
+++ LeanUWB/Class13.lean
@@ -1,7 +1,17 @@
 import LeanUWB.Class02
 
 
+lemma reversAppend_eq_rever {T : Type} (x y : List T) :
+    reversAppend x y = rever y ++ x := by
+  induction y generalizing x with
+  | nil =>
+    rfl
+  | cons d l ih =>
+    simp [rever, reversAppend]
+    apply ih
+
 theorem revers_eq_rever (T : Type) : @revers T = @rever T := by
   ext1 l
-  -- Let's first see a naïve proof attempt...
-  sorry
+  unfold revers
+  rw [reversAppend_eq_rever]
+  apply List.append_nil
