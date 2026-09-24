2148e540c53f5c1dfe2a1bcdf61c2155f9e6fa2b
Martin Dvorak <martin.dvorak@matfyz.cz>
2026-09-24 17:16:48 +0200

    helping students by stating which lemmas will be useful for proving the final theorems

--- LeanUWB/Exercises16.lean
+++ LeanUWB/Exercises16.lean
@@ -8,6 +8,28 @@ noncomputable instance : Bot A where
 noncomputable instance : Top A where
   top := ⊔ Set.univ
 
+lemma bot_under (x : A) : ⊥ ⊑ x := by
+  sorry
+
+lemma top_above (x : A) : x ⊑ ⊤ := by
+  sorry
+
+lemma above_iff_supre_pair (a b : A) :
+    a ⊑ b ↔ ⊔ {a, b} = b := by
+  sorry
+
+lemma under_iff_infim_pair (a b : A) :
+    a ⊑ b ↔ ⊓ {a, b} = a := by
+  sorry
+
+lemma JoinContinuous.monoton {F : A → A} (hF : JoinContinuous F) :
+    Monoton F := by
+  sorry
+
+lemma MeetContinuous.monoton {F : A → A} (hF : MeetContinuous F) :
+    Monoton F := by
+  sorry
+
 theorem JoinContinuous.constructLeastFixpoint {F : A → A} (hF : JoinContinuous F) :
     LeastFixpoint F (⊔ { F^[i] ⊥ | i : ℕ }) := by
   sorry
--- README.md
+++ README.md
@@ -38,7 +38,7 @@ madv@kma.zcu.cz
 * [2026-09-03](LeanUWB/Class13.lean) : induction generalizing
 * [2026-09-10](LeanUWB/Class14.lean) : structures, classes, and notation
 * [2026-09-17](LeanUWB/Class15.lean) : posets
-* 2026-09-25 at 16:00 : constructions of fixed points
+* [2026-09-25](LeanUWB/Exercises16.lean) : constructions of fixed points
 * 2026-10-01 : TBA
 * 2026-10-08 : TBA
 * 2026-10-15 : TBA
